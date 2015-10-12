519ab268f33618d600642ce6641d74b54bbe9c5a
owlowlgo <k@gnn.io>
2015-10-12 18:34:36 +0900

    Completed basic functionality.

--- redterm.py
+++ redterm.py
@@ -3,7 +3,7 @@ import logging
 import reddit
 import terminal
 
-logging.basicConfig(filename="sample.log", level=logging.DEBUG, filemode="w" )
+logging.basicConfig(filename="redterm.debug.log", level=logging.DEBUG, filemode="w")
 
 
 def main():
@@ -11,27 +11,46 @@ def main():
     reddit_io = reddit.IO()
 
     with terminal_io.setup():
-        subreddit_title = 'worldnews'
-        submissions = reddit_io.get_submissions(subreddit_title, 100)
-        terminal_io.set_submissions(submissions)
+        subreddit_title = 'frontpage'
+        page = terminal.PageSubreddit(reddit_io.get_submissions(subreddit_title, 100))
+        terminal_io.pages.append(page)
 
         while True:
-            terminal_io.render_submissions()
+            page_current = terminal_io.pages[-1]
+
+            terminal_io.render(page_current)
 
             key_pressed = terminal_io.get_key(1)
-            if key_pressed.code == terminal_io.terminal.KEY_UP:
-                terminal_io.selected_menu -= 1
-            elif key_pressed.code == terminal_io.terminal.KEY_DOWN:
-                terminal_io.selected_menu += 1
+            if key_pressed.code == terminal.KEY_UP:
+                page_current.item_selected -= 1
 
-            if not key_pressed:
-                pass
+            elif key_pressed.code == terminal.KEY_DOWN:
+                page_current.item_selected += 1
 
-            if key_pressed.code == terminal_io.terminal.KEY_ESCAPE:
-                logging.debug("This is a debug message")
+            elif key_pressed.code == terminal.KEY_PGUP:
+                page_current.item_selected = terminal_io.get_out_of_screen_item_loc_prev(page_current)
+
+            elif key_pressed.code == terminal.KEY_PGDN:
+                page_current.item_selected = terminal_io.get_out_of_screen_item_loc_next(page_current)
+
+            elif key_pressed.code == terminal.KEY_ENTER:
+                submission_selected = page_current.items[page_current.item_selected]
+                new_page = terminal.PageSubmission([submission_selected] + reddit_io.get_comments(submission_selected))
+
+                terminal_io.pages.append(new_page)
+                terminal_io.render_buffer = []
+
+            elif key_pressed.code == terminal.KEY_BACKSPACE:
+                del terminal_io.pages[-1]
+                terminal_io.render_buffer = []
+
+            elif key_pressed.code == terminal.KEY_ESCAPE:
                 break
 
-    terminal_io.cleanup()
+            elif not key_pressed:
+                pass
+
+            #logging.debug(key_pressed.code)
 
 if __name__ == '__main__':
     main()
--- terminal.py
+++ terminal.py
@@ -1,99 +1,211 @@
 import contextlib
 import logging
+import re
+import signal
+import unicodedata
 
 import blessed
 from uniseg.wrap import tt_wrap
+
+terminal = blessed.Terminal()
+
+# Key codes
+KEY_DOWN = 258
+KEY_UP = 259
+KEY_LEFT = 260
+KEY_RIGHT = 261
+KEY_BACKSPACE = 330
+KEY_PGDN = 338
+KEY_PGUP = 339
+KEY_ENTER = 343
+KEY_ESCAPE = 361
 
 
 class IO:
     """Wrapper for Blessed to render Reddit"""
     def __init__(self):
-        self.terminal = blessed.Terminal()
-        print(self.terminal.enter_fullscreen)
-        print(self.terminal.clear)
+        self.pages = []          # List of all Page-related objects generated for session
 
-        self.submissions = []         # Raw list of Submission objects
-        self.submission_display = []  # Submission information to display(Entire page)
+        self.render_buffer = []  # Immediate content to display on terminal
+        self.render_offset = 0   # Offset to keep track of where in submission_display to render from
 
-        self.render_buffer = []                    # Content to display on terminal
-        self.render_offset = 0                     # Offset to keep track of where in submission_display to render from
-        self.render_width = self.terminal.width    # Terminal width
-        self.render_height = self.terminal.height  # Terminal height
-        self.render_menu_y = []                    # Location of each submission item within submission_display
+        # Initialize terminal
+        print(terminal.enter_fullscreen)
+        print(terminal.clear)
+        signal.signal(signal.SIGWINCH, self.on_resize)
 
-        self._menu_selected = 0                    # Currently selected menu item
+    def on_resize(self, *args):
+        """Re-perform wrapping of text to accommodate new terminal size."""
+        self.render_buffer = self.pages[-1].item_displays_wrapped
+        self.render(self.pages[-1])
 
-    def set_submissions(self, submissions):
-        """Receive submission data and prepare it for rendering."""
-
-        self.submissions = submissions
-
-        for submission_no, submission in enumerate(self.submissions, 1):
-            self.submission_display.append(str(submission_no) + '. ' +
-                                         str(submission.title) + '\n' +
-                                         str(submission.score) + 'pts ' +
-                                         str(submission.num_comments) + ' comments by (' +
-                                         str(submission.author) + ') - /r/' +
-                                         str(submission.subreddit))
-
-        self.__wrap_submissions(2)
-
-    def render_submissions(self):
+    def render(self, page):
         """Render subreddit while maintaining key press updates and resizing."""
+        # Remember terminal size
+        terminal_width = terminal.width
+        terminal_height = terminal.height - 1
 
-        # Redo submission display info wrapping if resize is detected.
-        if self.render_height != self.terminal.height or self.render_width != self.terminal.width:
-            self.render_width = self.terminal.width
-            self.render_height = self.terminal.height
-            self.__wrap_submissions(2)
+        # Fill buffer with content if empty
+        if not self.render_buffer:
+            self.render_buffer = page.item_displays_wrapped
 
         # Adjust the rendering offset if selected menu item is out of bounds of current terminal.
-        if self.render_menu_y[self._menu_selected] >= self.render_offset + self.render_height - 1:
-            self.render_offset += self.render_height - 1
-        elif self.render_menu_y[self._menu_selected] < self.render_offset:
-            self.render_offset -= self.render_height - 1
+        if page.item_displays_wrapped_locs[page.item_selected] >= self.render_offset + terminal_height:
+            self.render_offset += terminal_height
+        elif page.item_displays_wrapped_locs[page.item_selected] < self.render_offset:
+            self.render_offset -= terminal_height
             if self.render_offset < 0:
                 self.render_offset = 0
 
-        # Clear terminal
-        print(self.terminal.clear)
-
         # Render buffer content to terminal
-        for buffer_no, buffer_line in enumerate(self.render_buffer[self.render_offset:self.render_offset + self.render_height - 1]):
-            print(self.terminal.move(buffer_no, 2) + buffer_line)
+        for buffer_line_no in range(terminal_height):
+            try:
+                buffer_line = self.render_buffer[self.render_offset + buffer_line_no]
+                print(terminal.move(buffer_line_no, 0) + buffer_line)
+            except IndexError:
+                # Print blank line in case buffer is empty
+                print(terminal.move(buffer_line_no, 0) + ' ' * terminal_width)
 
         # Render cursor.
-        print(self.terminal.move(self.render_menu_y[self._menu_selected] - self.render_offset, 0) + '>')
+        string = ''
+        try:
+            string = '-' * (page.item_indentations[page.item_selected] * 4)
+        except IndexError:
+            string = ''
 
-    def __wrap_submissions(self, tab=0):
-        """Wrap all strings to fit in current terminal screen size."""
-        self.render_menu_y = []
-        self.render_buffer = []
+        print(terminal.move(page.item_displays_wrapped_locs[page.item_selected] - self.render_offset, 0) + '>' + string)
 
-        line_no = 0
-        for submission_info in self.submission_display:
-            self.render_menu_y.append(line_no)
-            for line in tt_wrap(submission_info, self.terminal.width - tab):
-                self.render_buffer.append(line.rstrip('\n'))
-                line_no += 1
+    @staticmethod
+    def get_out_of_screen_item_loc_next(page):
+        new_loc = page.item_displays_wrapped_locs[page.item_selected] + terminal.height
+        closest_item_loc = min(range(len(page.item_displays_wrapped_locs)), key=lambda i: abs(page.item_displays_wrapped_locs[i]-new_loc))
 
-    @property
-    def selected_menu(self):
-        return self._menu_selected
+        return closest_item_loc
 
-    @selected_menu.setter
-    def selected_menu(self, potential_selected_menu):
-        if 0 <= potential_selected_menu < len(self.submissions):
-            self._menu_selected = potential_selected_menu
+    @staticmethod
+    def get_out_of_screen_item_loc_prev(page):
+        new_loc = page.item_displays_wrapped_locs[page.item_selected] - terminal.height
+        closest_item_loc = min(range(len(page.item_displays_wrapped_locs)), key=lambda i: abs(page.item_displays_wrapped_locs[i]-new_loc))
+
+        return closest_item_loc
 
     @contextlib.contextmanager
     def setup(self):
-        with self.terminal.cbreak(), self.terminal.hidden_cursor():
-            yield
+        try:
+            with terminal.cbreak(), terminal.hidden_cursor():
+                yield
+        finally:
+            print(terminal.clear)
+            print(terminal.exit_fullscreen)
 
-    def get_key(self, timeout=1):
-        return self.terminal.inkey(timeout=timeout)
+    @staticmethod
+    def get_key(timeout=0):
+        return terminal.inkey(timeout=timeout)
 
-    def cleanup(self):
-        print(self.terminal.clear)
-        print(self.terminal.exit_fullscreen)
+
+class Page:
+    """Base class for how items are to be displayed and selected."""
+    def __init__(self, items, tab=2):
+        self.items = items
+        self.item_displays = []
+        self._item_displays_wrapped = []
+        self.item_displays_wrapped_locs = []
+        self._item_selected = 0
+        self.item_indentations = []
+
+        self.tab = tab
+
+    @property
+    def item_displays_wrapped(self):
+        """pass."""
+        self._item_displays_wrapped = []
+        self.item_displays_wrapped_locs = []
+
+        terminal_width = terminal.width
+
+        line_no = 0
+        for item_no, item_display in enumerate(self.item_displays):
+
+            try:
+                item_indentation = self.item_indentations[item_no] * 4
+            except IndexError:
+                item_indentation = 0
+            finally:
+                indentation = self.tab + item_indentation
+
+            self.item_displays_wrapped_locs.append(line_no)
+            for line in tt_wrap(item_display, terminal_width - indentation):
+                line = ' ' * indentation + line.rstrip('\n') + ' ' * (terminal_width - self.get_screen_length(line) - indentation)
+                self._item_displays_wrapped.append(line)
+                line_no += 1
+
+        return self._item_displays_wrapped
+
+    @property
+    def item_selected(self):
+        return self._item_selected
+
+    @item_selected.setter
+    def item_selected(self, potential_item_selected):
+        if 0 <= potential_item_selected < len(self.items):
+            self._item_selected = potential_item_selected
+
+    @staticmethod
+    def get_screen_length(string):
+        """Returns on-screen length of given string"""
+        return len(string) + sum(unicodedata.east_asian_width(char) in {'W', 'F', 'A'} for char in string)
+
+
+class PageSubreddit(Page):
+    """Holds information on how to display subreddit."""
+    def __init__(self, items):
+        Page.__init__(self, items)
+
+        for item_no, item in enumerate(self.items, 1):
+            self.item_displays.append(str(item_no) + '. ' +
+                                      str(item.title) + '\n' +
+                                      str(item.score) + 'pts ' +
+                                      str(item.num_comments) + ' comments by (' +
+                                      str(item.author) + ') - /r/' +
+                                      str(item.subreddit))
+
+
+class PageSubmission(Page):
+    """Holds information on how to display a submission along with comments."""
+    def __init__(self, items):
+        Page.__init__(self, items)
+
+        self.item_displays.append(str(items[0].title) + '\n' +
+                                  str(items[0].score) + 'pts ' +
+                                  str(items[0].num_comments) + ' comments by (' +
+                                  str(items[0].author) + '\n\n' +
+                                  str(re.sub('\n\s*\n', '\n\n', items[0].selftext)) + '\n' +
+                                  '=' * terminal.width)
+
+        self.item_indentations = self._get_comment_depth(items[0], items[1:])
+
+        for item_no, item in enumerate(self.items[1:], start=1):
+            try:
+                self.item_displays.append(str(item.author) + ' - ' +
+                                          str(item.score) + 'pts \n' +
+                                          str(item.body))
+
+            except AttributeError:
+                self.item_displays.append('More comments...')
+
+    @staticmethod
+    def _get_comment_depth(submission, comments):
+        comment_depth = [0]
+        comment_indentation_depth = 0
+        comment_indentation_depth_ids = [submission.id]
+        for comment in comments:
+            if comment.parent_id[3:] in comment_indentation_depth_ids:
+                comment_indentation_depth = comment_indentation_depth_ids.index(comment.parent_id[3:])
+                comment_indentation_depth_ids = comment_indentation_depth_ids[0:comment_indentation_depth + 1]
+            else:
+                comment_indentation_depth += 1
+                comment_indentation_depth_ids.append(comment.parent_id[3:])
+            comment_depth.append(comment_indentation_depth)
+        return comment_depth
+
+
