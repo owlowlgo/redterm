44aecc1b3660293e68238344bbe8b7c5f2a10e9e
owlowlgo <owlowlgo@users.noreply.github.com>
2015-10-12 18:34:36 +0900

    Completed basic functionality.

--- reddit.py
+++ reddit.py
@@ -4,8 +4,9 @@ import praw
 class IO:
     """Wrapper for PRAW to provide functionality to help rendering on console easier"""
     def __init__(self):
-        self.reddit = praw.Reddit(user_agent='redterm')
+        self.reddit = praw.Reddit(user_agent='User-Agent: python:redterm:v0.0.1')
 
+    # Retrieve submissions and return list
     def get_submissions(self, name_subreddit, limit=50):
         submissions = []
         for submission in self.reddit.get_subreddit(name_subreddit).get_hot(limit=limit):
@@ -13,6 +14,7 @@ class IO:
 
         return submissions
 
+    # Retrieve comments of submission and return list
     def get_comments(self, submission):
         comments = []
         for comment in praw.helpers.flatten_tree(submission.comments):
@@ -20,20 +22,12 @@ class IO:
 
         return comments
 
-    def get_comment_depth(self, submission):
-        comment_depth = []
-        comment_indentation_depth = 0
-        comment_indentation_depth_ids = [submission.id]
-        comments = praw.helpers.flatten_tree(submission.comments)
-        for comment in comments:
-            if comment.parent_id[3:] in comment_indentation_depth_ids:
-                comment_indentation_depth = comment_indentation_depth_ids.index(comment.parent_id[3:])
-                comment_indentation_depth_ids = comment_indentation_depth_ids[0:comment_indentation_depth + 1]
-            else:
-                comment_indentation_depth += 1
-                comment_indentation_depth_ids.append(comment.parent_id[3:])
-            comment_depth.append(comment_indentation_depth)
-        return comment_depth
+    def is_page_subreddit(self, page):
+        return type(page[0]) is praw.objects.Submission
+
+    def is_page_submission(self, page):
+        return type(page[0]) is praw.objects.Comment
+
 
 if __name__ == '__main__':
     reddit_io = IO()
